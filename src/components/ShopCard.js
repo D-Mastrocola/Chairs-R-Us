a32a4d4cc460713735c6dce26ec0a430d3373194
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-01-15 23:37:20 -0500

    update

--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -1,10 +1,15 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 
 function ShopCard(props) {
+  let cardStyle = {
+    backgroundImage: "url(" + props.imgSrc + ")"
+  };
+  console.log(cardStyle.backgroundImage);
   return(
     <div className='shop-card'>
-      <img src={props.imgSrc}/>
+      <div className='shop-card-img' style={cardStyle}>
+
+      </div>
       <ul>
         <li>{props.name}</li>
         <li>${props.price}.00</li>
