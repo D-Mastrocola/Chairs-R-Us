4ae41bd66a112cdcf6444a880136d5a039b7aa0d
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-01-15 23:14:23 -0500

    update

--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -1,13 +1,13 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 
-function ShopCard() {
+function ShopCard(props) {
   return(
     <div className='shop-card'>
-      <img src='#'/>
+      <img src={props.imgSrc}/>
       <ul>
-        <li></li>
-        <li>Price</li>
+        <li>{props.name}</li>
+        <li>${props.price}.00</li>
       </ul>
     </div>
   )
