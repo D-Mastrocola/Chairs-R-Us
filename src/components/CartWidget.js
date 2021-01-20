996e6fc46604fcce297a3ebf0f54ddc57b580c1a
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-01-19 23:10:20 -0500

    update

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -8,7 +8,7 @@ function CartWidget(props) {
       <h1>Cart</h1>
       <ul className='cart-widget-list'>
         {props.cart.map((e, index) =>
-          <li key={index.toString()}>{e.name}</li>
+          <li className='cart-widget-list-item' key={index.toString()}><img src={e.imgSrc}/> <h4>{e.name}</h4></li>
         )}
       </ul>
     </div>
