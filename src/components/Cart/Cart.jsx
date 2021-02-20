ed01a3b443e95b2697d641cb6b4433f8bef494fd
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-02-20 18:42:18 -0500

    Cart UI
    Built the Cart UI. It sill has no functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import {Container, Typography, Button, Grid} from '@material-ui/core';
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
 
-const Cart = () => {
+import useStyles from "./styles";
+import CartItem from './CartItem/CartItem';
+
+const Cart = ({ cart }) => {
+  console.log(cart);
+  const classes = useStyles();
+
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">The cart is empty.</Typography>
+  );
+  
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem item={item}/>
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">Subtotal: {cart.subtotal.formmatted_with_symbol}</Typography>
+        <div>
+            <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
+            <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
+        </div>
+      </div>
+    </>
+  );
+
+  if(!cart.line_items) return 'loading...';
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3" gutterBottom>Your Cart </Typography>
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
 
-export default ;
+export default Cart;
